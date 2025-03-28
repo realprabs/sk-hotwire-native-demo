5b0c537a625b8997c3186e87bb3f4bd6f18f9ee4
Prabhakar Bhat <prabhakar.bhat@stablemoney.in>
2025-03-29 00:38:22 +0530

    Added long page content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,12 @@
 	import '../app.css';
 	import type { LayoutProps } from './$types';
 	import clsx from 'clsx';
+    import { ProgressBar } from "@prgm/sveltekit-progress-bar";
 
 	let { children, data }: LayoutProps = $props();
 </script>
+
+<ProgressBar displayThresholdMs={500} />
 
 <div
 	class={clsx('prose prose-stone mx-auto max-w-screen-md px-4 py-6', {
